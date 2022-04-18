471bf34f78d230a77e5efc24ff4b3633e649edfa
RobPasMue <roberto.pastormuela@ansys.com>
2022-04-18 16:39:50 +0200

    First gRPC tests (not running yet)

--- python/grpc/client.py
+++ python/grpc/client.py
@@ -11,8 +11,13 @@ from python.grpc.server import __NP_DTYPE_TO_DATATYPE
 class DemoGRPCClient:
     """The API Eigen Example client class for interacting via gRPC."""
 
-    def __init__(self, ip="127.0.0.1", port=50051, timeout=1):
+    def __init__(self, ip="127.0.0.1", port=50051, timeout=1, test=None):
         """Initialize connection to the API Eigen server"""
+        # For test purposes, provide a stub directly
+        if test is not None:
+            self._stub = test
+            return
+
         self._stub = None
         self._channel_str = "%s:%d" % (ip, port)
 
---
+++ python/grpc/test/test_grpc.py
@@ -0,0 +1,50 @@
+import pytest
+
+import numpy as np
+
+from python.grpc.client import DemoGRPCClient
+
+# ================================================================================
+# Point your stubs and service to test the client-server interaction
+#
+# These fixtures will provide us with the capability of doing client-server tests
+# ================================================================================
+
+
+@pytest.fixture(scope="module")
+def grpc_add_to_server():
+    from protos.src.python.grpcdemo_pb2_grpc import add_GRPCDemoServicer_to_server
+
+    return add_GRPCDemoServicer_to_server
+
+
+@pytest.fixture(scope="module")
+def grpc_servicer():
+    from python.grpc.server import GRPCDemoServicer
+
+    return GRPCDemoServicer()
+
+
+@pytest.fixture(scope="module")
+def grpc_stub(grpc_channel):
+    from protos.src.python.grpcdemo_pb2_grpc import GRPCDemoStub
+
+    return GRPCDemoStub(grpc_channel)
+
+
+# ================================================================================
+# Unit tests for client-server interaction
+# ================================================================================
+
+
+def test_add_vectors(grpc_stub):
+    """Unit test to verify that the client gets the expected response
+    when performing the addition of two numpy arrays (as vectors)."""
+
+    client = DemoGRPCClient(test=grpc_stub)
+
+    vec_1 = np.array([1, 2, 3, 4], dtype=np.float64)
+    vec_2 = np.array([5, 4, 2, 0], dtype=np.float64)
+
+    vec_add = client.add_vectors(vec_1, vec_2)
+    np.testing.assert_allclose(vec_add, np.array([6, 6, 5, 4]))
