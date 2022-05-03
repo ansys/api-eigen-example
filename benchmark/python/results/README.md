728b9bcf08b9431bb1af120778e45804fb7bc764
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2022-05-03 16:38:19 +0200

    Include BM tests and results (#16)
    * Reorder benchmark folder
    
    * First version of BM tests
    
    * Final tweaks to BM test scripts
    
    * First results
    
    * Pre-commit fixes
    
    * Show BM results in a README file
    
    * Update README.md

---
+++ benchmark/python/results/README.md
@@ -0,0 +1,25 @@
+# Benchmark test results
+
+### Add vectors (gRPC)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_add_vectors_grpc.svg?raw=true)
+
+### Add vectors (REST)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_add_vectors_rest.svg?raw=true)
+
+### Multiply vectors (gRPC)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_multiply_vectors_grpc.svg?raw=true)
+
+### Multiply vectors (REST)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_multiply_vectors_rest.svg?raw=true)
+
+### Add matrices (gRPC)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_add_matrices_grpc.svg?raw=true)
+
+### Add matrices (REST)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_add_matrices_rest.svg?raw=true)
+
+### Multiply matrices (gRPC)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_multiply_matrices_grpc.svg?raw=true)
+
+### Multiply matrices (REST)
+![alt text](https://github.com/ansys/api-eigen-example/blob/feat/bm-results/benchmark/python/results/hist-test_multiply_matrices_rest.svg?raw=true)
---
+++ benchmark/python/tests/test_grpc.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pytest
+
+from ansys.eigen.python.grpc.client import DemoGRPCClient
+
+# ================================================================================
+# Point your stubs and service to test the client-server interaction
+#
+# These fixtures will provide us with the capability of doing client-server tests
+# ================================================================================
+
+
+@pytest.fixture(scope="module")
+def grpc_add_to_server():
+    from ansys.eigen.python.grpc.generated.grpcdemo_pb2_grpc import (
+        add_GRPCDemoServicer_to_server,
+    )
+
+    return add_GRPCDemoServicer_to_server
+
+
+@pytest.fixture(scope="module")
+def grpc_servicer():
+    from ansys.eigen.python.grpc.server import GRPCDemoServicer
+
+    return GRPCDemoServicer()
+
+
+@pytest.fixture(scope="module")
+def grpc_stub(grpc_channel):
+    from ansys.eigen.python.grpc.generated.grpcdemo_pb2_grpc import GRPCDemoStub
+
+    return GRPCDemoStub(grpc_channel)
+
+
+# ================================================================================
+# Unit tests for client-server interaction
+# ================================================================================
+
+
+def test_add_vectors_grpc(benchmark, grpc_stub):
+    """BM test to measure the time consumed so that the client gets the expected response
+    when performing the addition of two numpy arrays (as vectors)."""
+
+    client = DemoGRPCClient(test=grpc_stub)
+
+    vec_1 = np.array([1, 2, 3, 4], dtype=np.float64)
+    vec_2 = np.array([5, 4, 2, 0], dtype=np.float64)
+
+    benchmark(client.add_vectors, vec_1, vec_2)
+
+
+def test_multiply_vectors_grpc(benchmark, grpc_stub):
+    """BM test to measure the time consumed so that the client gets the expected response
+    when performing the multiplication of two numpy arrays (as vectors)."""
+
+    client = DemoGRPCClient(test=grpc_stub)
+
+    vec_1 = np.array([1, 2, 3, 4], dtype=np.float64)
+    vec_2 = np.array([5, 4, 2, 0], dtype=np.float64)
+
+    benchmark(client.multiply_vectors, vec_1, vec_2)
+
+
+def test_add_matrices_grpc(benchmark, grpc_stub):
+    """BM test to measure the time consumed so that the client gets the expected response
+    when performing the addition of two numpy arrays (as matrices)."""
+
+    client = DemoGRPCClient(test=grpc_stub)
+
+    mat_1 = np.array([[1, 2], [3, 4]], dtype=np.float64)
+    mat_2 = np.array([[5, 4], [2, 0]], dtype=np.float64)
+
+    benchmark(client.add_matrices, mat_1, mat_2)
+
+
+def test_multiply_matrices_grpc(benchmark, grpc_stub):
+    """BM test to measure the time consumed so that the client gets the expected response
+    when performing the multiplication of two numpy arrays (as matrices)."""
+    client = DemoGRPCClient(test=grpc_stub)
+
+    mat_1 = np.array([[1, 2], [3, 4]], dtype=np.float64)
+    mat_2 = np.array([[5, 4], [2, 0]], dtype=np.float64)
+
+    benchmark(client.multiply_matrices, mat_1, mat_2)
