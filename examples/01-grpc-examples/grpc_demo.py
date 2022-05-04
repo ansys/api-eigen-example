3573ec874b74f2da440cb2c84da935b4766374c3
RobPasMue <roberto.pastormuela@ansys.com>
2022-05-04 07:58:12 +0200

    Pre-commit changes

--- examples/01-grpc-examples/grpc_demo.py
+++ examples/01-grpc-examples/grpc_demo.py
@@ -39,10 +39,10 @@ from ansys.eigen.python.grpc.client import DemoGRPCClient
 # together with the connection itself, the formatting of the request and so on. When constructing the class we
 # must provide as inputs the ``ip`` and the ``port`` of the server. For this demo we are running, since we
 # already deployed the server (either manually or as a container), we will provide the following arguments:
-# 
+#
 # - IP(or DNS): 127.0.0.1
 # - Port: 50051
-# 
+#
 # The server is exposed by **IP 127.0.0.1** and **port 50051** as per defined in the Dockerfile of the server and
 # the server itself. Thus, the previous inputs should be provided, although they are also the default values. Nonetheless,
 # in the IP field we could also provide the DNS for the sake of showing that DNS values are also accepted. In this case,
@@ -75,24 +75,24 @@ vec_2 = np.array([5, 4, 2, 0], dtype=np.float64)
 # Performing gRPC interaction
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Now, we will call the client method ``add_vectors(...)``, and we will explain the typical process of all interface methods (``add_XXXX(...)`` and ``multiply_XXXX(...)``):
-# 
+#
 # - The client performs some sanity checks to confirm that the inputs provided are as expected. This Demo has some limitations such as: only 1D, 2D numpy.ndarrays are allowed; they must be of type float64. Direct interaction with the server (i.e. without a client) and using gRPC is out of the scope of this demo. Doing so could be considered as "impossible" since you would have to serialize your message on your own, following the standard defined in the proto files.
-# 
+#
 # - The client serializes the messages with the inputs provided using generator functions. Our end server is characterized for receiving ``streams`` of messages, which basically represent a list of messages. Each of these messages are serialized following the interface proposed by the proto files, thanks to the automatically generated source code by protobuf.
 #     - For example our ``Vector`` message is characterized for having the following structure:
-#     
+#
 #         ``` enum DataType{INTEGER = 0;DOUBLE = 1;} ```
-#           
+#
 #         ``` message Vector {DataType data_type = 1; int32 vector_size = 2; bytes vector_as_chunk = 3;} ```
-# 
+#
 # - When the server receives the messages, it deserializes them and interprets each of the previous fields. Thus, it is easily converted into a numpy.ndarray of the adequate type. Then, the desired vectors to be added are passed to the Eigen library via our ``demo_eigen_wrapper`` for the resolution of the demanded operation.
-# 
+#
 # - Once the results of the operation are available, the server serializes the result and responds with the adequate message to the client.
 #     - For example, according to the proto file, our server receives a stream of Vector messages, and returns a single Vector message (which contains the result of the requested operation):
 #         ``` rpc AddVectors(stream Vector) returns (Vector) {} ```
-# 
+#
 # - The client then receives the response, deserializes the message and returns the corresponding result to the end-user as numpy.ndarray. Thus, the entire process is like a black-box for the end-user, and does not require to understand what is happening behind the scenes, since the end-user is only interested in the end-result.
-# 
+#
 # Let us now call the method!
 
 # Call the client to perform your desired operation
