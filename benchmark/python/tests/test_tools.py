609485556e908a3c4b1e39c79bad14ddebb73fa8
RobPasMue <roberto.pastormuela@ansys.com>
2022-05-05 10:10:00 +0200

    Configurable test size

--- benchmark/python/tests/test_tools.py
+++ benchmark/python/tests/test_tools.py
@@ -4,12 +4,24 @@ from random import random, seed
 
 import numpy as np
 
+# Modifiable parameter -- NVALUES
+#
+# This parameter defines the amount of runs (and consequently, powers of 2)
+# which will define the size of our arrays.. See below, SIZES
+NVALUES = 8
+
+# ==================================================================================
+# IT IS NOT RECOMMENDED TO MODIFY THE SCRIPT BEYOND THIS POINT WITHOUT KNOWLEDGE
+# OF HOW THE BENCHMARK TESTS ARE RUN. USE WITH CAUTION...
+# ==================================================================================
+
+
 # MIN and MAX values for our random numbers
 MIN = 0.0
 MAX = 10.0
 
 # The amount of sizes of numpy.ndarrays to be processed
-SIZES = [pow(2, n) for n in range(1, 8)]
+SIZES = [pow(2, n) for n in range(1, NVALUES)]
 SIZES_IDS = [f"{i:05d}" for i in SIZES]
 
 # Initialize our seed
