b559ce0917cb249669692b94e1b6341aa3bb2939
RobPasMue <roberto.pastormuela@ansys.com>
2022-05-17 17:25:11 +0200

    Implement basic serializing funcs + FlipVector + SayHello

--- src/ansys/eigen/cpp/grpc/server/src/GRPCService.cpp
+++ src/ansys/eigen/cpp/grpc/server/src/GRPCService.cpp
@@ -1,5 +1,6 @@
 #include "GRPCService.hpp"
 
+#include <exception>
 #include <iostream>
 
 // ============================================================================
@@ -24,11 +25,35 @@ ansys::grpc::service::GRPCService::~GRPCService() {
 
 ::grpc::Status ansys::grpc::service::GRPCService::SayHello(
     ::grpc::ServerContext* context, const ::grpcdemo::HelloRequest* request,
-    ::grpcdemo::HelloReply* response) {}
+    ::grpcdemo::HelloReply* response) {
+    // Log event
+    std::cout << "Greeting requested! Requested by " << request->name()
+              << std::endl;
+
+    // Define the response!
+    response->set_message("Hello, " + request->name() + "!");
+    return ::grpc::Status::OK;
+}
 
 ::grpc::Status ansys::grpc::service::GRPCService::FlipVector(
     ::grpc::ServerContext* context, const ::grpcdemo::Vector* request,
-    ::grpcdemo::Vector* response) {}
+    ::grpcdemo::Vector* response) {
+    // Log event
+    std::cout << "Vector flip requested!" << std::endl;
+
+    // First, deserialize our vector into an Eigen::VectorXd object
+    auto vec = deserialize_vector(request->vector_as_chunk(),
+                                  request->vector_size(), request->data_type());
+
+    // Flip the vector
+    Eigen::VectorXd flip_vec = vec.reverse();
+
+    // Send the response
+    response->set_data_type(request->data_type());
+    response->set_vector_size(request->vector_size());
+    response->set_vector_as_chunk(serialize_vector(flip_vec));
+    return ::grpc::Status::OK;
+}
 
 ::grpc::Status ansys::grpc::service::GRPCService::AddVectors(
     ::grpc::ServerContext* context,
@@ -53,6 +78,131 @@ ansys::grpc::service::GRPCService::~GRPCService() {
 // ============================================================================
 // GRPCService PRIVATE METHODS
 // ============================================================================
+
+Eigen::VectorXd ansys::grpc::service::GRPCService::deserialize_vector(
+    const std::string& bytes, const int length, grpcdemo::DataType type) {
+    // Get the step of the bytes array to interpret each type
+    int step{0};
+    switch (type) {
+        case grpcdemo::DataType::DOUBLE:
+            step = sizeof(double);
+            break;
+        case grpcdemo::DataType::INTEGER:
+            step = sizeof(int);
+            break;
+        default:
+            throw std::invalid_argument("Invalid Vector type!");
+    }
+
+    // Turn into a const char*
+    auto ptr = bytes.data();
+    int idx{0};
+
+    // Interpret the stream of bytes
+    Eigen::VectorXd vector{};
+    vector.resize(length);
+    while (idx < length) {
+        // Decode first (depending on data type)
+        switch (type) {
+            case grpcdemo::DataType::DOUBLE:
+                vector[idx] = std::atof(ptr);
+                break;
+            case grpcdemo::DataType::INTEGER:
+                vector[idx] = std::atoi(ptr);
+                break;
+        }
+
+        // Advance to next position
+        ptr += step;
+        idx++;
+    }
+
+    return vector;
+}
+
+std::string ansys::grpc::service::GRPCService::serialize_vector(
+    const Eigen::VectorXd& vector) {
+    std::string vec_as_str{};
+
+    for (int idx = 0; idx < vector.size(); ++idx) {
+        // We will assume that all elements are of type double
+        const unsigned char* ptr =
+            reinterpret_cast<const unsigned char*>(&vector[idx]);
+        for (size_t i = 0; i < sizeof(double); ++i)
+            vec_as_str.push_back(ptr[i]);
+    }
+
+    return vec_as_str;
+}
+
+Eigen::MatrixXd ansys::grpc::service::GRPCService::deserialize_matrix(
+    const std::string& bytes, const int rows, const int cols,
+    grpcdemo::DataType type) {
+    // Get the step of the bytes array to interpret each type
+    int step{0};
+    switch (type) {
+        case grpcdemo::DataType::DOUBLE:
+            step = sizeof(double);
+            break;
+        case grpcdemo::DataType::INTEGER:
+            step = sizeof(int);
+            break;
+        default:
+            throw std::invalid_argument("Invalid Matrix type!");
+    }
+
+    // Turn into a const char*
+    auto ptr = bytes.data();
+    int idx{0};
+
+    // Interpret the stream of bytes
+    Eigen::MatrixXd matrix{};
+    matrix.resize(rows, cols);
+    int row_idx{0}, col_idx{0};
+
+    while (idx < rows * cols) {
+        // Decode first (depending on data type)
+        switch (type) {
+            case grpcdemo::DataType::DOUBLE:
+                matrix(row_idx, col_idx) = std::atof(ptr);
+                break;
+            case grpcdemo::DataType::INTEGER:
+                matrix(row_idx, col_idx) = std::atoi(ptr);
+                break;
+        }
+
+        // Move the row/col indices
+        if (col_idx < cols) {
+            col_idx++;
+        } else {
+            col_idx = 0;
+            row_idx++;
+        }
+
+        // Advance to next position
+        ptr += step;
+        idx++;
+    }
+
+    return matrix;
+}
+
+std::string ansys::grpc::service::GRPCService::serialize_matrix(
+    const Eigen::MatrixXd& matrix) {
+    std::string mat_as_str{};
+
+    for (int row_idx = 0; row_idx < matrix.rows(); ++row_idx) {
+        for (int col_idx = 0; col_idx < matrix.cols(); ++col_idx) {
+            // We will assume that all elements are of type double
+            const unsigned char* ptr = reinterpret_cast<const unsigned char*>(
+                &matrix(row_idx, col_idx));
+            for (size_t i = 0; i < sizeof(double); ++i)
+                mat_as_str.push_back(ptr[i]);
+        }
+    }
+
+    return mat_as_str;
+}
 
 // ============================================================================
 // ansys::grpc::server NAMESPACE METHODS
--- src/ansys/eigen/cpp/grpc/server/src/GRPCService.hpp
+++ src/ansys/eigen/cpp/grpc/server/src/GRPCService.hpp
@@ -1,5 +1,7 @@
 #include <grpcpp/grpcpp.h>
 
+#include <eigen3/Eigen/Dense>
+
 #include "generated/grpcdemo.grpc.pb.h"
 
 #ifndef SRC_ANSYS_EIGEN_CPP_GRPC_SERVER_SRC_GRPCSERVICE_HPP
@@ -29,34 +31,124 @@ class GRPCService final : public grpcdemo::GRPCDemo::Service {
      */
     ~GRPCService();
 
+    /**
+     * @brief Method to provide a simple greeting to the client.
+     *
+     * @param context the gRPC Server context.
+     * @param request the gRPC request.
+     * @param response the gRPC response this method will fill.
+     * @return ::grpc::Status
+     */
     ::grpc::Status SayHello(::grpc::ServerContext* context,
                             const ::grpcdemo::HelloRequest* request,
                             ::grpcdemo::HelloReply* response);
 
+    /**
+     * @brief Method to provide a flipped vector to the client.
+     *
+     * @param context the gRPC Server context.
+     * @param request the gRPC request.
+     * @param response the gRPC response this method will fill.
+     * @return ::grpc::Status
+     */
     ::grpc::Status FlipVector(::grpc::ServerContext* context,
                               const ::grpcdemo::Vector* request,
                               ::grpcdemo::Vector* response);
 
+    /**
+     * @brief Method to provide the addition of Vector messages.
+     *
+     * @param context the gRPC Server context.
+     * @param reader the reader for the stream of requests.
+     * @param response the gRPC response this method will fill.
+     * @return ::grpc::Status
+     */
     ::grpc::Status AddVectors(::grpc::ServerContext* context,
                               ::grpc::ServerReader< ::grpcdemo::Vector>* reader,
                               ::grpcdemo::Vector* response);
 
+    /**
+     * @brief Method to provide the dot product of Vector messages.
+     *
+     * @param context the gRPC Server context.
+     * @param reader the reader for the stream of requests.
+     * @param response the gRPC response this method will fill.
+     * @return ::grpc::Status
+     */
     ::grpc::Status MultiplyVectors(
         ::grpc::ServerContext* context,
         ::grpc::ServerReader< ::grpcdemo::Vector>* reader,
         ::grpcdemo::Vector* response);
 
+    /**
+     * @brief Method to provide the addition of Matrix messages.
+     *
+     * @param context the gRPC Server context.
+     * @param reader the reader for the stream of requests.
+     * @param response the gRPC response this method will fill.
+     * @return ::grpc::Status
+     */
     ::grpc::Status AddMatrices(
         ::grpc::ServerContext* context,
         ::grpc::ServerReader< ::grpcdemo::Matrix>* reader,
         ::grpcdemo::Matrix* response);
 
+    /**
+     * @brief Method to provide the multiplication of Matrix messages.
+     *
+     * @param context the gRPC Server context.
+     * @param reader the reader for the stream of requests.
+     * @param response the gRPC response this method will fill.
+     * @return ::grpc::Status
+     */
     ::grpc::Status MultiplyMatrices(
         ::grpc::ServerContext* context,
         ::grpc::ServerReader< ::grpcdemo::Matrix>* reader,
         ::grpcdemo::Matrix* response);
 
    private:
+    /**
+     * @brief Method used to deserialize a Vector message into an
+     * Eigen::VectorXd object.
+     *
+     * @param bytes : the chunk of bytes from where the vector is deserialized.
+     * @param length : the length of the vector we are deserializing.
+     * @param type : the type of data inside the vector (e.g. double, int...).
+     * @return Eigen::VectorXd
+     */
+    Eigen::VectorXd deserialize_vector(const std::string& bytes,
+                                       const int length,
+                                       grpcdemo::DataType type);
+    /**
+     * @brief Method used to serialize an Eigen::VectorXd object into a Vector
+     * message.
+     *
+     * @param vector : the Eigen::VectorXd to be serialized.
+     * @return std::string
+     */
+    std::string serialize_vector(const Eigen::VectorXd& vector);
+
+    /**
+     * @brief Method used to deserialize a Matrix message into an
+     * Eigen::MatrixXd object.
+     *
+     * @param bytes : the chunk of bytes from where the matrix is deserialized.
+     * @param rows : the number of rows of the matrix we are deserializing.
+     * @param cols : the number of columns of the matrix we are deserializing.
+     * @param type : the type of data inside the matrix (e.g. double, int...).
+     * @return Eigen::MatrixXd
+     */
+    Eigen::MatrixXd deserialize_matrix(const std::string& bytes, const int rows,
+                                       const int cols, grpcdemo::DataType type);
+
+    /**
+     * @brief Method used to serialize an Eigen::MatrixXd object into a Matrix
+     * message.
+     *
+     * @param vector : the Eigen::MatrixXd to be serialized.
+     * @return std::string
+     */
+    std::string serialize_matrix(const Eigen::MatrixXd& matrix);
 };
 
 }  // namespace service
