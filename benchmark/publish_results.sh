6ab7b25135d2f313a6c3f62f9ab38293f93d7e40
RobPasMue <roberto.pastormuela@ansys.com>
2022-05-20 16:32:20 +0200

    Last BM results

--- benchmark/publish_results.sh
+++ benchmark/publish_results.sh
@@ -2,4 +2,4 @@
 
 # after running the "run_tests.sh", upload the figures (to both the static folder and the docs folder)
 \cp -r hist/* results/
-\cp -r hist/* ../../doc/source/benchmark/images/
+\cp -r hist/* ../doc/source/benchmark/images/
