2714645f29c2993dc2ae61a516d5f47c79b74a98
RobPasMue <roberto.pastormuela@ansys.com>
2022-05-26 11:35:00 +0200

    Updates to processing script

--- benchmark/data/process_data.py
+++ benchmark/data/process_data.py
@@ -123,11 +123,10 @@ for (test_type, x_axis) in zip(test_types, x_axis_tests):
     )
     plt.tight_layout()
     figure.savefig("%s.svg" % test_type, format="svg")
-    
+
     # convert y-axis to Logarithmic scale
     plt.yscale("log")
     plt.tight_layout()
     figure.savefig("%s_log.svg" % test_type, format="svg")
-    
 
     print("Plot for %s test saved!" % test_type)
