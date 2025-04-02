4c95d16d358f8ae3f48e0ea8c6ba37da20368e57
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-04-02 09:41:07 +0200

    fix: required changes (#257)
    * fix: missing requirements files
    
    * fix: pre-commit
    
    * fix: minimum CMAKE version
    
    * fix: conan version
    
    * fix: add override
    
    * fix: revert conanfiles

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,11 @@
+# Contributors
+
+## Project Lead
+
+* [Roberto Pastor](https://github.com/RobPasMue)
+
+## Individual Contributors
+
+* [Alexander Kaszynski](https://github.com/akaszynski)
+* [Kathy Pippert](https://github.com/PipKat)
+* [Maxime Rey](https://github.com/MaxJPRey)
--- src/ansys/eigen/python/grpc/run_codegen.py
+++ src/ansys/eigen/python/grpc/run_codegen.py
@@ -1,5 +1,6 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
 # Copyright 2015 gRPC authors.
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
